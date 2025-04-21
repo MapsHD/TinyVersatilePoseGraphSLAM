3f19b760e8359442af23564ecca8d5016231e7d8
janusz <januszbedkowski@gmail.com>
2025-04-21 12:45:08 +0200

    initial commit

---
+++ examples/example.cpp
@@ -0,0 +1,7 @@
+int main(int argc, char *argv[]){
+
+    return 0;
+}
+
+// git submodule add https://gitlab.com/libeigen/eigen.git eigen
+// git submodule add https://github.com/freeglut/freeglut.git freeglut
